1f34d392888c7f23f376cbd47fb5e1c546d2c444
KillTheSw1tch <zaharpetrenko525@gmail.com>
2025-07-17 20:44:28 +0200

    updated link

--- src/pages/registrationPage.jsx
+++ src/pages/registrationPage.jsx
@@ -97,7 +97,7 @@ function RegistrationPage() {
     };
     console.log("Payload:", JSON.stringify(payload, null, 2));
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/user/register/', {
+      const response = await fetch(import.meta.env.VITE_API_URL + '/user/register/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
