f2d28f1a822fc885157e652da2e1472b7bd4a13a
KillTheSw1tch <zaharpetrenko525@gmail.com>
2025-07-19 17:24:33 +0200

    added dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
 
-// Підвантажимо .env (якщо використовуєш локально .env-файл)
-dotenv.config()
+const API_URL = process.env.VITE_API_URL || '/'
 
-// Тут точно беремо з process.env
-const API_URL = process.env.VITE_API_URL || 'http://127.0.0.1:8000/api'
-
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/',         // тут зазвичай лишають '/' для Netlify
+  base: '/',
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(API_URL)
+  },
   server: {
-    host: 'localhost',
-    port: 5174,
     proxy: {
-      // всі запити /api/* під час dev перенаправляти на бек
       '/api': {
         target: API_URL,
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '/api')
+        changeOrigin: true
       }
-    },
-  },
-  define: {
-    // щоб в твоєму коді import.meta.env.VITE_API_URL теж працювало
-    'import.meta.env.VITE_API_URL': JSON.stringify(API_URL)
+    }
   }
 })
