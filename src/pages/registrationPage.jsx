66abc64162e3ae20f7492fdc544a497b1fc2a25d
KillTheSw1tch <130804033+KillTheSw1tch@users.noreply.github.com>
2025-07-19 20:37:44 +0000

    Updated regPage

--- src/pages/registrationPage.jsx
+++ src/pages/registrationPage.jsx
@@ -97,7 +97,7 @@ function RegistrationPage() {
     };
     console.log("Payload:", JSON.stringify(payload, null, 2));
     try {
-      const response = await fetch(import.meta.env.VITE_API_URL + '/user/register/', {
+      const response = await fetch(import.meta.env.VITE_API_URL + 'api/user/register/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
